3f3960174e81c90e3adfc6486346e455d60bbc01
Annika Hägglund <annika.a.a.hagglund@outlook.com>
2022-01-12 11:12:18 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+let username;
+let password;
+
+document.addEventListener("submit", (event) => {
+  console.log("Submitted");
+  username = document.getElementById("username").value;
+  password = document.getElementById("password").value;
+
+  const headers = new Headers();
+  headers.append("Accept", "application/json");
+  headers.append("Authorization", btoa(username + ":" + password));
+
+  fetch("http://localhost:3000/", { headers: headers })
+    .then(function (response) {
+      if (response.ok) {
+        document.write("SUCCESSFUL LOGIN");
+      }
+      throw response;
+    })
+    .then(function (data) {
+      console.log(data);
+    })
+    .catch(function (error) {
+      console.warn(error);
+    });
+
+  event.preventDefault();
+});
